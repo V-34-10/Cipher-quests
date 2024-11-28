2aaaa0114427643272c7620d2b7de693a174265b
Vlad <vladandryschenko98@gmail.com>
2024-11-28 22:36:29 +0200

    MenuActivity: add controlButtons, clickGameButton, ClickButton, finishApp

--- app/src/main/java/com/ciphero/questa/ui/menu/MenuActivity.kt
+++ app/src/main/java/com/ciphero/questa/ui/menu/MenuActivity.kt
@@ -1,21 +1,74 @@
 package com.ciphero.questa.ui.menu
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.ciphero.questa.R
+import com.ciphero.questa.databinding.ActivityMenuBinding
+import com.ciphero.questa.ui.games.SceneBasicActivity
+import com.ciphero.questa.ui.privacy.PrivacyActivity
+import com.ciphero.questa.ui.settings.SettingsActivity
+import com.ciphero.questa.utils.DecoratorNavigationUI
+import kotlin.system.exitProcess
 
 class MenuActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMenuBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        DecoratorNavigationUI.hideNavigationBar(this)
+        controlButtons()
+    }
+
+    private fun controlButtons() {
+        val animator = AnimationUtils.loadAnimation(this@MenuActivity, R.anim.anim_scale)
+        binding.textPrivacy.setOnClickListener {
+            it.startAnimation(animator)
+            startActivity(Intent(this@MenuActivity, PrivacyActivity::class.java))
+            finish()
         }
+        binding.apply {
+            clickButton(btnSettings, SettingsActivity::class.java)
+            clickGameButton(btnFirstGame, R.string.first_game_btn)
+            clickGameButton(btnSecondGame, R.string.second_game_btn)
+            clickGameButton(btnThreeGame, R.string.three_game_btn)
+        }
+    }
+
+    private fun clickGameButton(button: View, gameName: Int) {
+        val animator = AnimationUtils.loadAnimation(this@MenuActivity, R.anim.anim_scale)
+        button.setOnClickListener {
+            it.startAnimation(animator)
+            this.getSharedPreferences("CipherQuestsPref", MODE_PRIVATE).edit()
+                .putString("nameGame", this.getString(gameName)).apply()
+            startActivity(Intent(this@MenuActivity, SceneBasicActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun <T : AppCompatActivity> clickButton(button: View, activityClass: Class<T>) {
+        val animator = AnimationUtils.loadAnimation(this@MenuActivity, R.anim.anim_scale)
+        button.setOnClickListener {
+            it.startAnimation(animator)
+            startActivity(Intent(this@MenuActivity, activityClass))
+            finish()
+        }
+    }
+
+    @Deprecated(
+        "Deprecated in Java",
+        ReplaceWith("super.onBackPressed()", "androidx.appcompat.app.AppCompatActivity")
+    )
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finishApp()
+    }
+
+    private fun finishApp() {
+        exitProcess(0)
     }
 }
