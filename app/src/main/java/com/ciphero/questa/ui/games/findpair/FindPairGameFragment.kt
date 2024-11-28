83bb60650f9103584f870ed7968ca32d18dae872
Vlad <vladandryschenko98@gmail.com>
2024-11-28 19:48:19 +0200

    UI FindPairGameFragment: add resources, control_status_bar: btn_pause/btn_back, scene_find_game, btn_okay

---
+++ app/src/main/java/com/ciphero/questa/ui/games/findpair/FindPairGameFragment.kt
@@ -0,0 +1,18 @@
+package com.ciphero.questa.ui.games.findpair
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ciphero.questa.R
+
+class FindPairGameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_find_pair_game, container, false)
+    }
+}
