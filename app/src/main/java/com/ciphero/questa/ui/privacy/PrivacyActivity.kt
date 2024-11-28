74ada6f9ca0234a9453069624b9ec58018ae989e
Vlad <vladandryschenko98@gmail.com>
2024-11-29 00:31:09 +0200

    PrivacyActivity: optimization: setupClickListeners, acceptPrivacyAndNavigate, openPrivacyLink,setPrivacyAccepted

--- app/src/main/java/com/ciphero/questa/ui/privacy/PrivacyActivity.kt
+++ app/src/main/java/com/ciphero/questa/ui/privacy/PrivacyActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.animation.AnimationUtils
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import com.ciphero.questa.R
 import com.ciphero.questa.databinding.ActivityPrivacyBinding
 import com.ciphero.questa.ui.daily.DailyRewardActivity
@@ -14,56 +15,46 @@ import com.ciphero.questa.utils.DecoratorNavigationUI
 
 class PrivacyActivity : AppCompatActivity() {
     private val binding by lazy { ActivityPrivacyBinding.inflate(layoutInflater) }
+    private val preferences by lazy { getSharedPreferences("CipherQuestsPref", MODE_PRIVATE) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
         DecoratorNavigationUI.hideNavigationBar(this)
-        agreePrivacy()
-        runIntentPrivacyLink()
+        setupClickListeners()
     }
 
-    private fun setStatusAcceptPrivacy() =
-        this.getSharedPreferences("CipherQuestsPref", MODE_PRIVATE).edit()
-            .putBoolean("Privacy", true).apply()
-
-    private fun agreePrivacy() {
-        with(binding) {
-            buttonAcceptPrivacy.setOnClickListener { view ->
-                performClickAction(view) {
-                    setStatusAcceptPrivacy()
-                    launchActivity(DailyRewardActivity::class.java)
-                }
-            }
+    private fun setupClickListeners() {
+        binding.buttonAcceptPrivacy.setOnClickListener {
+            animateClick(it) { acceptPrivacyAndNavigate() }
+        }
+        binding.textRead.setOnClickListener {
+            animateClick(it) { openPrivacyLink() }
         }
     }
 
-    private fun runIntentPrivacyLink() {
-        with(binding) {
-            textRead.setOnClickListener { view ->
-                performClickAction(view) {
-                    startActivity(
-                        Intent(
-                            Intent.ACTION_VIEW,
-                            Uri.parse(getString(R.string.link_privacy))
-                        )
-                    )
-                }
-            }
-        }
+    private fun acceptPrivacyAndNavigate() {
+        setPrivacyAccepted()
+        navigateToActivity(DailyRewardActivity::class.java)
     }
 
-    private fun performClickAction(view: View, action: () -> Unit) {
+    private fun setPrivacyAccepted() = preferences.edit { putBoolean("Privacy", true) }
+
+    private fun openPrivacyLink() = startActivity(
+        Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.link_privacy)))
+    )
+
+    private fun animateClick(view: View, action: () -> Unit) {
         view.startAnimation(AnimationUtils.loadAnimation(this, R.anim.anim_scale))
         action()
     }
 
-    private fun <T> launchActivity(activityClass: Class<T>) {
+    private fun <T> navigateToActivity(activityClass: Class<T>) {
         startActivity(Intent(this, activityClass))
         finish()
     }
 
-    @Deprecated("Deprecated in Java")
+    @Deprecated("This method has been deprecated in favor of using the\n      {@link OnBackPressedDispatcher} via {@link #getOnBackPressedDispatcher()}.\n      The OnBackPressedDispatcher controls how back button events are dispatched\n      to one or more {@link OnBackPressedCallback} objects.")
     override fun onBackPressed() {
         super.onBackPressed()
         finish()
