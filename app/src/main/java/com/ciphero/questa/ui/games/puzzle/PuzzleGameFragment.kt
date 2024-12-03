e9891f53056016244080c5f830987e96a91d835a
Vlad <vladandryschenko98@gmail.com>
2024-12-03 02:42:45 +0200

    PuzzleGameFragment: add binding control button, set startRound

--- app/src/main/java/com/ciphero/questa/ui/games/puzzle/PuzzleGameFragment.kt
+++ app/src/main/java/com/ciphero/questa/ui/games/puzzle/PuzzleGameFragment.kt
@@ -1,18 +1,71 @@
 package com.ciphero.questa.ui.games.puzzle
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
 import com.ciphero.questa.R
+import com.ciphero.questa.adapters.PuzzleMoveListener
+import com.ciphero.questa.databinding.FragmentPuzzleGameBinding
+import com.ciphero.questa.ui.games.dialogs.DialogsBaseGame.startDialogVictoryGamePuzzle
+import com.ciphero.questa.ui.games.puzzle.animator.TimeBarAnimator
+import com.ciphero.questa.ui.menu.MenuActivity
 
-class PuzzleGameFragment : Fragment() {
+class PuzzleGameFragment : Fragment(), PuzzleMoveListener {
+    private var _binding: FragmentPuzzleGameBinding? = null
+    private val binding get() = _binding!!
+    private val scaleAnimation by lazy {
+        AnimationUtils.loadAnimation(
+            requireContext(),
+            R.anim.anim_scale
+        )
+    }
+    private lateinit var timer: TimeBarAnimator
+    private lateinit var gameGovern: GovernGamePuzzle
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_puzzle_game, container, false)
+    ): View {
+        _binding = FragmentPuzzleGameBinding.inflate(inflater, container, false)
+        gameGovern = GovernGamePuzzle
+        gameGovern.initGovernGamePuzzle(binding, requireContext())
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        timer = TimeBarAnimator(binding, requireContext())
+        gameGovern.startRound()
+
+        binding.btnPause.setOnClickListener {
+            it.startAnimation(scaleAnimation)
+            startDialogVictoryGamePuzzle(requireContext(), this)
+        }
+
+        binding.btnBack.setOnClickListener {
+            it.startAnimation(scaleAnimation)
+            startActivity(Intent(context, MenuActivity::class.java))
+            activity?.finish()
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.stopTimer()
+    }
+
+    override fun onMovePuzzle(move: Int) {
+        gameGovern.moveCount = move
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
