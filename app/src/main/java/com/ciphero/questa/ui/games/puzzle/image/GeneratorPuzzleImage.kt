47587e60f6bc32ff3410d5bf3834df46e95795c4
Vlad <vladandryschenko98@gmail.com>
2024-12-03 20:34:50 +0200

    GeneratorPuzzleImage: add model GridPuzzle, fix gridConfig

--- app/src/main/java/com/ciphero/questa/ui/games/puzzle/image/GeneratorPuzzleImage.kt
+++ app/src/main/java/com/ciphero/questa/ui/games/puzzle/image/GeneratorPuzzleImage.kt
@@ -2,19 +2,12 @@ package com.ciphero.questa.ui.games.puzzle.image
 
 import android.content.Context
 
-data class LevelPuzzleConfig(
-    val victoryListPuzzle: List<Int>,
-    val spanGrid: Int,
-    val idEndPuzzle: Int
-)
-
 class GeneratorPuzzleImage(private val context: Context) {
     private fun resIdForName(name: String): Int =
         context.resources.getIdentifier(name, "drawable", context.packageName)
 
-    private val easyPuzzleList = List(9) { resIdForName("puzzle_$it") }
-    val levelConfig = LevelPuzzleConfig(
-        victoryListPuzzle = easyPuzzleList,
+    val gridConfig = GridPuzzle(
+        victoryListPuzzle = List(9) { resIdForName("puzzle_$it") },
         spanGrid = 3,
         idEndPuzzle = 0
     )
---
+++ app/src/main/java/com/ciphero/questa/ui/games/puzzle/image/GridPuzzle.kt
@@ -0,0 +1,7 @@
+package com.ciphero.questa.ui.games.puzzle.image
+
+data class GridPuzzle(
+    val victoryListPuzzle: List<Int>,
+    val spanGrid: Int,
+    val idEndPuzzle: Int
+)
