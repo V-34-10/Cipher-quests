913ecdf17cca27536aff8e64284f54634ecebdba
Vlad <vladandryschenko98@gmail.com>
2024-12-03 02:40:39 +0200

    TimeBarAnimator: add startTimer, updateLineTimerWidth, stopTimer, resetTimer, resetLineTimerWidth

---
+++ app/src/main/java/com/ciphero/questa/ui/games/puzzle/animator/TimeBarAnimator.kt
@@ -0,0 +1,73 @@
+package com.ciphero.questa.ui.games.puzzle.animator
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.os.CountDownTimer
+import com.ciphero.questa.databinding.FragmentPuzzleGameBinding
+import com.ciphero.questa.ui.games.dialogs.DialogsBaseGame.startDialogVictoryGamePuzzle
+import com.ciphero.questa.ui.games.puzzle.PuzzleGameFragment
+
+class TimeBarAnimator(
+    private val binding: FragmentPuzzleGameBinding,
+    private val context: Context
+) {
+    private var timer: CountDownTimer? = null
+    private var startTime = 0L
+    private var elapsedTime = 0L
+    private val animationDuration = 30_000L
+    private var initialWidth = 0
+
+    private val lineTimer get() = binding.timerProgressBar.lineTimer
+
+    fun startTimer() {
+        if (initialWidth == 0) {
+            initialWidth = lineTimer.width
+        }
+        startTime = System.currentTimeMillis() - elapsedTime
+
+        timer = object : CountDownTimer(animationDuration, 1000L) {
+            override fun onTick(millisUntilFinished: Long) {
+                elapsedTime = System.currentTimeMillis() - startTime
+                updateLineTimerWidth(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                startDialogVictoryGamePuzzle(
+                    context,
+                    PuzzleGameFragment()
+                )
+            }
+        }.start()
+    }
+
+    private fun updateLineTimerWidth(millisUntilFinished: Long) {
+        val newWidth = (millisUntilFinished / animationDuration.toFloat() * initialWidth).toInt()
+        ValueAnimator.ofInt(lineTimer.width, newWidth).apply {
+            duration = 1000L
+            addUpdateListener {
+                val value = it.animatedValue as Int
+                val layoutParams = lineTimer.layoutParams
+                layoutParams.width = value
+                lineTimer.layoutParams = layoutParams
+            }
+            start()
+        }
+    }
+
+    fun stopTimer() {
+        timer?.cancel()
+        resetTimer()
+    }
+
+    private fun resetTimer() {
+        startTime = 0
+        elapsedTime = 0
+        resetLineTimerWidth()
+    }
+
+    private fun resetLineTimerWidth() {
+        val layoutParams = lineTimer.layoutParams
+        layoutParams.width = initialWidth
+        lineTimer.layoutParams = layoutParams
+    }
+}
