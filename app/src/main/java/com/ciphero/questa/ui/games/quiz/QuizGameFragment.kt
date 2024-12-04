260e46326b6ece8a3fccac161ec27cb4ed98829d
Vlad <vladandryschenko98@gmail.com>
2024-12-04 02:23:41 +0200

    QuizGameFragment/MenuActivity: fix startAnimation, add btnBack

--- app/src/main/java/com/ciphero/questa/ui/games/quiz/QuizGameFragment.kt
+++ app/src/main/java/com/ciphero/questa/ui/games/quiz/QuizGameFragment.kt
@@ -3,16 +3,19 @@ package com.ciphero.questa.ui.games.quiz
 import android.animation.Animator
 import android.animation.AnimatorInflater
 import android.animation.AnimatorListenerAdapter
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.ciphero.questa.R
 import com.ciphero.questa.databinding.FragmentQuizGameBinding
 import com.ciphero.questa.model.CasinoQuizQuestions
 import com.ciphero.questa.model.Question
+import com.ciphero.questa.ui.menu.MenuActivity
 import com.ciphero.questa.ui.settings.MusicControllerPlayer
 
 class QuizGameFragment : Fragment() {
@@ -21,6 +24,12 @@ class QuizGameFragment : Fragment() {
     private var currentQuestion: Question? = null
     private var selectedAnswerButton: TextView? = null
     private lateinit var musicSet: MusicControllerPlayer
+    private val scaleAnimation by lazy {
+        AnimationUtils.loadAnimation(
+            requireContext(),
+            R.anim.anim_scale
+        )
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -46,10 +55,16 @@ class QuizGameFragment : Fragment() {
         binding.btnThreeAnswer.setOnClickListener(answerClickListener)
 
         binding.btnNext.setOnClickListener {
+            it.startAnimation(scaleAnimation)
             selectedAnswerButton?.background = null
             selectedAnswerButton = null
             binding.btnNext.visibility = View.GONE
             showNextQuestion()
+        }
+        binding.btnBack.setOnClickListener {
+            it.startAnimation(scaleAnimation)
+            startActivity(Intent(requireContext(), MenuActivity::class.java))
+            activity?.finish()
         }
     }
 
