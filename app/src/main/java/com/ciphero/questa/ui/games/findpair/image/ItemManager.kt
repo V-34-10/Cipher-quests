3c4e6a605730682e6ce6f5349f51d2dc9a7d0c47
Vlad <vladandryschenko98@gmail.com>
2024-12-06 01:03:06 +0200

    ItemManager: optimizations: add generatePairs, setupPairItems

--- app/src/main/java/com/ciphero/questa/ui/games/findpair/image/ItemManager.kt
+++ app/src/main/java/com/ciphero/questa/ui/games/findpair/image/ItemManager.kt
@@ -2,45 +2,32 @@ package com.ciphero.questa.ui.games.findpair.image
 
 import com.ciphero.questa.model.FindPair
 
-class ItemManager(private val numPairs: Int = 4) {
-    /*private val numPairs: Int = 4
+class ItemManager(private val numPairs: Int) {
+
     private val pairList = mutableListOf<FindPair>()
-    private val imageList = mutableListOf<Int>()*/
 
-    /*fun setupPairItems() {
+    private fun generatePairs(images: List<Int> = GeneratorImageFindPair.getImages()): List<FindPair> {
+        val pairs = images.take(numPairs).flatMap { image ->
+            listOf(
+                FindPair(image, id = pairList.size),
+                FindPair(image, id = pairList.size + 1)
+            )
+        }.shuffled()
+
+        return if (images.size > numPairs) {
+            val uniqueImage = images[numPairs] // Get the next available image
+            (pairs + FindPair(uniqueImage, id = pairList.size)).shuffled()
+        } else {
+            pairs
+        }
+    }
+
+    fun setupPairItems(images: List<Int> = GeneratorImageFindPair.getImages()) {
         pairList.clear()
-        imageList.apply {
-            clear()
-            addAll(GeneratorImageFindPair.generateImageGrid())
-        }
-
-        var position = 0
-        repeat(numPairs) {
-            pairList.add(FindPair(imageList[it], id = position++))
-            pairList.add(FindPair(imageList[it], id = position++))
-        }
-
-        pairList.shuffle()
+        pairList.addAll(generatePairs(images))
     }
 
     fun getPairList(): List<FindPair> = pairList
 
     fun resetPairs() = pairList.forEach { it.reset() }
-
-    private fun FindPair.reset() {
-        flip = false
-        match = false
-    }*/
-
-    val imageList by lazy {
-        GeneratorImageFindPair.generateImageGrid().take(numPairs).flatMap { listOf(it, it) }
-            .shuffled()
-    }
-
-    fun getPairList(): List<FindPair> =
-        imageList.mapIndexed { index, img -> FindPair(img, id = index) }
-
-    fun resetPairs() {
-        getPairList().forEach { it.flip = false; it.match = false }
-    }
 }
