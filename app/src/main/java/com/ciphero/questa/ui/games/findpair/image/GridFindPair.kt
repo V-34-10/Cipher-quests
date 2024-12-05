d4f89f85e3fd4c9649596fb3d73592646f6a25c5
Vlad <vladandryschenko98@gmail.com>
2024-12-05 23:47:12 +0200

    Model: fix GridFindPair, add repeat

--- app/src/main/java/com/ciphero/questa/ui/games/findpair/image/GridFindPair.kt
+++ app/src/main/java/com/ciphero/questa/ui/games/findpair/image/GridFindPair.kt
@@ -3,5 +3,6 @@ package com.ciphero.questa.ui.games.findpair.image
 data class GridFindPair(
     val img: List<Int>,
     val row: Int,
-    val col: Int
+    val col: Int,
+    val repeat: Int = 2
 )
