e58e2c53f74e51b740e4138f72826c37d1e447f4
Vlad <vladandryschenko98@gmail.com>
2024-12-05 14:25:09 +0200

    GeneratorPuzzleImage: add preparationPuzzles: createInitialPuzzles, addFinalPuzzle

--- app/src/main/java/com/ciphero/questa/ui/games/puzzle/image/GeneratorPuzzleImage.kt
+++ app/src/main/java/com/ciphero/questa/ui/games/puzzle/image/GeneratorPuzzleImage.kt
@@ -1,6 +1,9 @@
 package com.ciphero.questa.ui.games.puzzle.image
 
+import android.annotation.SuppressLint
 import android.content.Context
+import com.ciphero.questa.R
+import com.ciphero.questa.model.Puzzle
 
 class GeneratorPuzzleImage(private val context: Context) {
     private fun resIdForName(name: String): Int =
@@ -11,4 +14,19 @@ class GeneratorPuzzleImage(private val context: Context) {
         spanGrid = 3,
         idEndPuzzle = 8
     )
+
+    @SuppressLint("DiscouragedApi")
+    fun preparationPuzzles(): MutableList<Puzzle> {
+        val initialPuzzles = createInitialPuzzles(gridConfig)
+        initialPuzzles.shuffle()
+        addFinalPuzzle(initialPuzzles, gridConfig)
+        return initialPuzzles
+    }
+
+    private fun createInitialPuzzles(levelConfig: GridPuzzle): MutableList<Puzzle> =
+        levelConfig.victoryListPuzzle.dropLast(1)
+            .mapIndexed { index, resourceId -> Puzzle(resourceId, index) }.toMutableList()
+
+    private fun addFinalPuzzle(puzzles: MutableList<Puzzle>, levelConfig: GridPuzzle) =
+        puzzles.add(Puzzle(R.drawable.puzzle8, levelConfig.idEndPuzzle))
 }
