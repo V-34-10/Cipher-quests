941fe12ddce75f276ffe743d143aec1a130c3912
Vlad <vladandryschenko98@gmail.com>
2024-12-05 14:02:39 +0200

    TimeBarAnimator: fix startTimer: fragment init;  animateLineTimer: add valueAnimator; update resetLineTimer

--- app/src/main/java/com/ciphero/questa/ui/games/puzzle/timer/TimeBarAnimator.kt
+++ app/src/main/java/com/ciphero/questa/ui/games/puzzle/timer/TimeBarAnimator.kt
@@ -2,27 +2,31 @@ package com.ciphero.questa.ui.games.puzzle.timer
 
 import android.animation.ValueAnimator
 import android.os.CountDownTimer
+import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import com.ciphero.questa.databinding.FragmentPuzzleGameBinding
 import com.ciphero.questa.ui.games.dialogs.DialogsBaseGame.startDialogLoseGameFindPair
 import com.ciphero.questa.ui.games.dialogs.DialogsBaseGame.startDialogLoseGamePuzzle
 import com.ciphero.questa.ui.games.findpair.controller.ControllerFindPairGame
-import com.ciphero.questa.ui.games.findpair.FindPairGameFragment
-import com.ciphero.questa.ui.games.puzzle.PuzzleGameFragment
 
 class TimeBarAnimator(
-    private val gameManager: ControllerFindPairGame?
+    private val gameManager: ControllerFindPairGame?,
+    private val binding: ViewBinding?
 ) {
     private var timer: CountDownTimer? = null
     private var startTime = 0L
     private var elapsedTime = 0L
     private val animationDuration = 30_000L
     private var initialWidth = 0
+    private var fragment: Fragment? = null
+    private var valueAnimator: ValueAnimator? = null
 
-    fun startTimer(binding: ViewBinding) {
+    fun startTimer(fragment: Fragment) {
+        this.fragment = fragment
         if (initialWidth == 0) {
             when (binding) {
-                is FragmentPuzzleGameBinding -> initialWidth = binding.timerProgressBar.lineTimer.width
+                is FragmentPuzzleGameBinding -> initialWidth =
+                    binding.timerProgressBar.lineTimer.width
             }
         }
         startTime = System.currentTimeMillis() - elapsedTime
@@ -31,56 +35,64 @@ class TimeBarAnimator(
             override fun onTick(millisUntilFinished: Long) {
                 elapsedTime = System.currentTimeMillis() - startTime
                 when (binding) {
-                    is FragmentPuzzleGameBinding ->  updateLineTimerWidth(binding, millisUntilFinished)
+                    is FragmentPuzzleGameBinding -> animateLineTimer(
+                        binding,
+                        millisUntilFinished
+                    )
                 }
             }
 
             override fun onFinish() {
-                when (binding) {
-                    is FragmentPuzzleGameBinding ->
-                        startDialogLoseGamePuzzle(PuzzleGameFragment())
-
-                    else -> startDialogLoseGameFindPair(
-                        FindPairGameFragment(),
-                        gameManager
-                    )
+                fragment.let { frag ->
+                    if (frag.isAdded) {
+                        when (binding) {
+                            is FragmentPuzzleGameBinding -> startDialogLoseGamePuzzle(frag)
+                            else -> startDialogLoseGameFindPair(
+                                frag,
+                                gameManager
+                            )
+                        }
+                    }
                 }
-
             }
         }.start()
     }
 
-    private fun updateLineTimerWidth(
+    private fun animateLineTimer(
         binding: FragmentPuzzleGameBinding,
         millisUntilFinished: Long
     ) {
         val newWidth = (millisUntilFinished / animationDuration.toFloat() * initialWidth).toInt()
-        ValueAnimator.ofInt(binding.timerProgressBar.lineTimer.width, newWidth).apply {
-            duration = 1000L
-            addUpdateListener {
-                val value = it.animatedValue as Int
-                val layoutParams = binding.timerProgressBar.lineTimer.layoutParams
-                layoutParams.width = value
-                binding.timerProgressBar.lineTimer.layoutParams = layoutParams
+        valueAnimator?.cancel()
+        valueAnimator =
+            ValueAnimator.ofInt(binding.timerProgressBar.lineTimer.width, newWidth).apply {
+                duration = 1000L
+                addUpdateListener {
+                    val value = it.animatedValue as Int
+                    val layoutParams = binding.timerProgressBar.lineTimer.layoutParams
+                    layoutParams.width = value
+                    binding.timerProgressBar.lineTimer.layoutParams = layoutParams
+                }
+                start()
             }
-            start()
-        }
     }
 
     fun stopTimer(binding: FragmentPuzzleGameBinding) {
         timer?.cancel()
+        valueAnimator?.cancel()
         resetTimer(binding)
     }
 
     private fun resetTimer(binding: FragmentPuzzleGameBinding) {
         startTime = 0
         elapsedTime = 0
-        resetLineTimerWidth(binding)
+        resetLineTimer(binding)
     }
 
-    private fun resetLineTimerWidth(binding: FragmentPuzzleGameBinding) {
+    private fun resetLineTimer(binding: FragmentPuzzleGameBinding) {
+        val defaultWidth = (150 * binding.root.context.resources.displayMetrics.density).toInt()
         val layoutParams = binding.timerProgressBar.lineTimer.layoutParams
-        layoutParams.width = initialWidth
+        layoutParams.width = defaultWidth
         binding.timerProgressBar.lineTimer.layoutParams = layoutParams
     }
 }
