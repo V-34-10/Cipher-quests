d4c46cc3498fb5d419caba7b0cf7371ce6b451a7
Vlad <vladandryschenko98@gmail.com>
2024-12-05 14:34:41 +0200

    GovernGamePuzzle: fix initGovernGamePuzzle, optimization setAdapter

--- app/src/main/java/com/ciphero/questa/ui/games/puzzle/controller/GovernGamePuzzle.kt
+++ app/src/main/java/com/ciphero/questa/ui/games/puzzle/controller/GovernGamePuzzle.kt
@@ -3,12 +3,11 @@ package com.ciphero.questa.ui.games.puzzle.controller
 import android.annotation.SuppressLint
 import android.content.Context
 import androidx.recyclerview.widget.GridLayoutManager
-import com.ciphero.questa.R
 import com.ciphero.questa.adapters.puzzle.PuzzleGameAdapter
 import com.ciphero.questa.databinding.FragmentPuzzleGameBinding
-import com.ciphero.questa.model.Puzzle
-import com.ciphero.questa.ui.games.puzzle.timer.TimeBarAnimator
+import com.ciphero.questa.ui.games.puzzle.PuzzleGameFragment
 import com.ciphero.questa.ui.games.puzzle.image.GeneratorPuzzleImage
+import com.ciphero.questa.ui.games.puzzle.timer.TimeBarAnimator
 
 @SuppressLint("StaticFieldLeak")
 object GovernGamePuzzle {
@@ -16,15 +15,19 @@ object GovernGamePuzzle {
     private lateinit var timer: TimeBarAnimator
     private lateinit var binding: FragmentPuzzleGameBinding
     private lateinit var context: Context
+    private lateinit var fragment: PuzzleGameFragment
     var moveCount = 0
 
     fun initGovernGamePuzzle(
         binding: FragmentPuzzleGameBinding,
-        context: Context
+        context: Context,
+        fragment: PuzzleGameFragment,
+        timer: TimeBarAnimator
     ) {
         GovernGamePuzzle.binding = binding
         GovernGamePuzzle.context = context
-        timer = TimeBarAnimator(null)
+        GovernGamePuzzle.timer = timer
+        GovernGamePuzzle.fragment = fragment
     }
 
     fun startRound() {
@@ -37,40 +40,20 @@ object GovernGamePuzzle {
         setAdapter(binding, context)
     }
 
-    @SuppressLint("DiscouragedApi")
-    private fun getShuffledPuzzles(context: Context): MutableList<Puzzle> {
-        val puzzleImageSetup = GeneratorPuzzleImage(context)
-        val levelConfig = puzzleImageSetup.gridConfig
-
-        val resourcesPuzzles = levelConfig.victoryListPuzzle.dropLast(1)
-        val sectorPuzzle =
-            resourcesPuzzles.mapIndexed { index, resourceId -> Puzzle(resourceId, index) }
-                .toMutableList()
-
-        sectorPuzzle.shuffle()
-        sectorPuzzle.add(
-            Puzzle(
-                R.drawable.puzzle_0,
-                levelConfig.idEndPuzzle
-            )
-        )
-        return sectorPuzzle
-    }
-
     private fun setAdapter(
         binding: FragmentPuzzleGameBinding,
         context: Context
     ) {
-        val puzzleImageSetup = GeneratorPuzzleImage(context)
-        val levelConfig = puzzleImageSetup.gridConfig
+        val puzzleImage = GeneratorPuzzleImage(context)
         puzzleAdapter = PuzzleGameAdapter(
             binding.sceneCard,
-            getShuffledPuzzles(context),
+            puzzleImage.preparationPuzzles(),
             context,
             timer,
-            binding
+            fragment
         )
-        binding.sceneCard.layoutManager = GridLayoutManager(context, levelConfig.spanGrid)
+        binding.sceneCard.layoutManager =
+            GridLayoutManager(context, puzzleImage.gridConfig.spanGrid)
         binding.sceneCard.adapter = puzzleAdapter
     }
 }
