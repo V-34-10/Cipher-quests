cde9d5c8f8169d4e2ba6c1682fb6e0992e19013a
Vlad <vladandryschenko98@gmail.com>
2024-12-05 23:39:02 +0200

    Model: update FindPair, add methods flip/match/reset; add general structure PairData

--- app/src/main/java/com/ciphero/questa/model/FindPair.kt
+++ app/src/main/java/com/ciphero/questa/model/FindPair.kt
@@ -1,8 +1,23 @@
 package com.ciphero.questa.model
 
+data class PairData(val pair: FindPair, val position: Int)
+
 data class FindPair(
     val img: Int,
     var flip: Boolean = false,
     var match: Boolean = false,
     var id: Int = -1
-)
+) {
+    fun flip() {
+        flip = !flip
+    }
+
+    fun match() {
+        match = true
+    }
+
+    fun reset() {
+        flip = false
+        match = false
+    }
+}
