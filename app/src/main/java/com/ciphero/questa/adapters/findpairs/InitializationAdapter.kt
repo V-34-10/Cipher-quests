ace5637ae45af867e31965cc7c1e956972e29046
Vlad <vladandryschenko98@gmail.com>
2024-12-05 23:46:14 +0200

    InitializationAdapter: optimizations methods

--- app/src/main/java/com/ciphero/questa/adapters/findpairs/InitializationAdapter.kt
+++ app/src/main/java/com/ciphero/questa/adapters/findpairs/InitializationAdapter.kt
@@ -1,23 +1,29 @@
 package com.ciphero.questa.adapters.findpairs
 
 import android.annotation.SuppressLint
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.ciphero.questa.databinding.FragmentFindPairGameBinding
 import com.ciphero.questa.model.FindPair
 
-class InitializationAdapter {
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: FindPairGameAdapter
+class InitializationAdapter(var binding: FragmentFindPairGameBinding) {
+    private val recyclerView: RecyclerView by lazy {
+        binding.sceneFindGame.apply {
+            layoutManager = GridLayoutManager(context, 3)
+        }
+    }
+    private var adapter: FindPairGameAdapter? = null
 
     fun setAdapter(findPairList: List<FindPair>) {
         adapter = FindPairGameAdapter(findPairList).also { recyclerView.adapter = it }
     }
 
     @SuppressLint("NotifyDataSetChanged")
-    fun resetAdapter() = adapter.notifyDataSetChanged()
+    fun resetAdapter() = adapter?.notifyDataSetChanged()
 
-    fun notifyItemChanged(position: Int) = adapter.notifyItemChanged(position)
+    fun notifyItemChanged(position: Int) = adapter?.notifyItemChanged(position)
 
     fun setItemClickListener(onClick: (FindPair, Int) -> Unit) {
-        adapter.onClick = onClick
+        adapter?.onClick = onClick
     }
 }
