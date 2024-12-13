75aca15905ee6919c59a56fcc56a53423804d088
Vlad <vladandryschenko98@gmail.com>
2024-12-13 16:53:23 +0200

    NetworkCheck: optimization checkAccessInternet

--- app/src/main/java/com/ciphero/questa/utils/NetworkCheck.kt
+++ app/src/main/java/com/ciphero/questa/utils/NetworkCheck.kt
@@ -3,14 +3,21 @@ package com.ciphero.questa.utils
 import android.annotation.SuppressLint
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 
 object NetworkCheck {
 
     @SuppressLint("ObsoleteSdkInt")
     fun checkAccessInternet(context: Context): Boolean {
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetworkInfo = connectivityManager.activeNetworkInfo
-        return activeNetworkInfo != null && (activeNetworkInfo.isConnected || activeNetworkInfo.isConnectedOrConnecting)
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            return networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
+        } else {
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            return activeNetworkInfo?.isConnectedOrConnecting == true
+        }
     }
 }
