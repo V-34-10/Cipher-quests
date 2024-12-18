c20356b6a7f53722048c752df688708245e9e6ff
Vlad <vladandryschenko98@gmail.com>
2024-12-19 00:05:11 +0200

    PreferencesManager: optimization methods

--- app/src/main/java/com/ciphero/questa/utils/PreferencesManager.kt
+++ app/src/main/java/com/ciphero/questa/utils/PreferencesManager.kt
@@ -58,27 +58,30 @@ object PreferencesManager {
 
     fun getRewardCurrentDay(context: Context) =
         context.getSharedPreferences(context.getString(R.string.app_name_pref), MODE_PRIVATE)
-            .getInt("currentDay", 1)
+            .getInt(context.getString(R.string.key_current_day_pref), 1)
 
     fun setRewardCurrentDay(context: Context, day: Int) =
         context.getSharedPreferences(context.getString(R.string.app_name_pref), MODE_PRIVATE)
-            .edit().putInt("currentDay", day).apply()
+            .edit().putInt(context.getString(R.string.key_current_day_pref), day).apply()
 
     fun getLastRewardShownDate(context: Context) =
         context.getSharedPreferences(context.getString(R.string.app_name_pref), MODE_PRIVATE)
-            .getString("lastRewardShownDate", null)
+            .getString(context.getString(R.string.key_last_reward_shown_date_pref), null)
 
     fun setLastRewardShownDate(context: Context, date: SimpleDateFormat) =
         context.getSharedPreferences(context.getString(R.string.app_name_pref), MODE_PRIVATE)
-            .edit().putString("lastRewardShownDate", date.format(Date())).apply()
+            .edit().putString(
+                context.getString(R.string.key_last_reward_shown_date_pref),
+                date.format(Date())
+            ).apply()
 
     fun getLastRewardDay(context: Context) =
         context.getSharedPreferences(context.getString(R.string.app_name_pref), MODE_PRIVATE)
-            .getString("lastRewardDay", null)
+            .getString(context.getString(R.string.key_last_reward_day_pref), null)
 
     fun setLastRewardDay(context: Context, currentDate: String) =
         context.getSharedPreferences(context.getString(R.string.app_name_pref), MODE_PRIVATE).edit()
-            .putString("lastRewardDay", currentDate).apply()
+            .putString(context.getString(R.string.key_last_reward_day_pref), currentDate).apply()
 
 
     fun resetRewardProgress(context: Context, date: SimpleDateFormat) {
@@ -86,11 +89,11 @@ object PreferencesManager {
             context.getSharedPreferences(context.getString(R.string.app_name_pref), MODE_PRIVATE)
                 .edit()
         ) {
-            putInt("currentDay", 1)
-            putString("lastRewardDay", date.format(Date()))
-            putBoolean("day1Rewarded", false)
-            putBoolean("day2Rewarded", false)
-            putBoolean("day3Rewarded", false)
+            putInt(context.getString(R.string.key_current_day_pref), 1)
+            putString(context.getString(R.string.key_last_reward_day_pref), date.format(Date()))
+            putBoolean(context.getString(R.string.key_day1_rewarded_pref), false)
+            putBoolean(context.getString(R.string.key_day2_rewarded_pref), false)
+            putBoolean(context.getString(R.string.key_day3_rewarded_pref), false)
             commit()
         }
     }
@@ -106,9 +109,9 @@ object PreferencesManager {
 
     fun setStatusOfferwall(context: Context, status: Boolean) =
         context.getSharedPreferences(context.getString(R.string.app_name_pref), MODE_PRIVATE).edit()
-            .putBoolean("StatusOffer", status).apply()
+            .putBoolean(context.getString(R.string.key_status_offer_pref), status).apply()
 
     fun getStatusOfferwall(context: Context) =
         context.getSharedPreferences(context.getString(R.string.app_name_pref), MODE_PRIVATE)
-            .getBoolean("StatusOffer", false)
+            .getBoolean(context.getString(R.string.key_status_offer_pref), false)
 }
