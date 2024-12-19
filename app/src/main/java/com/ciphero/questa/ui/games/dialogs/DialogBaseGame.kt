09d032c8dfa02f2aabdbf054b95ae117f76a707d
Vlad <vladandryschenko98@gmail.com>
2024-12-19 23:58:03 +0200

    DialogsBaseGame: fix support old version Api hide UI

--- app/src/main/java/com/ciphero/questa/ui/games/dialogs/DialogBaseGame.kt
+++ app/src/main/java/com/ciphero/questa/ui/games/dialogs/DialogBaseGame.kt
@@ -1,19 +1,18 @@
 package com.ciphero.questa.ui.games.dialogs
 
+import android.app.Activity
 import android.app.Dialog
 import android.content.Context
-import android.graphics.Color
-import android.os.Build
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowInsets
-import android.view.WindowInsetsController
-import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import com.ciphero.questa.R
 import com.ciphero.questa.ui.games.findpair.controller.ControllerFindPairGame
 import com.ciphero.questa.ui.games.puzzle.controller.GovernGamePuzzle.restartRound
 import com.ciphero.questa.utils.AnimatorManager.startAnimateClickButton
+import com.ciphero.questa.utils.DecoratorNavigationUI
 import com.ciphero.questa.utils.DecoratorNavigationUI.navigateToWithFragment
 
 object DialogsBaseGame {
@@ -26,34 +25,20 @@ object DialogsBaseGame {
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT
             )
-
-            window?.let { window ->
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                    window.setDecorFitsSystemWindows(false)
-                    val controller = window.insetsController
-                    controller?.let {
-                        it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-                        it.systemBarsBehavior =
-                            WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-                    }
-                } else {
-                    window.setFlags(
-                        WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                        WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-                    )
-                    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-                    window.statusBarColor = Color.TRANSPARENT
-                }
-            }
         }
     }
 
     private fun showDialog(
+        activity: Activity,
         fragment: Fragment,
         layoutResId: Int,
         onDismiss: () -> Unit?
     ) {
         val dialog = createDialog(fragment.requireContext(), layoutResId)
+        dialog.show()
+        Handler(Looper.getMainLooper()).postDelayed({
+            DecoratorNavigationUI.hideNavigationBar(activity)
+        }, 100)
         dialog.findViewById<View>(R.id.btn_next)?.setOnClickListener {
             startAnimateClickButton(it, fragment.requireContext())
             dialog.dismiss()
@@ -64,39 +49,53 @@ object DialogsBaseGame {
             dialog.dismiss()
             navigateToWithFragment(fragment)
         }
-        dialog.show()
     }
 
-    fun startDialogLoseGameFindPair(fragment: Fragment, gameManager: ControllerFindPairGame?) =
+    fun startDialogLoseGameFindPair(
+        fragment: Fragment,
+        gameManager: ControllerFindPairGame?
+    ) =
         showDialog(
+            fragment.requireActivity(),
             fragment, R.layout.dialog_game_find_pair_lose
         ) { gameManager?.stopGame() }
 
-    fun startDialogPauseGameFindPair(fragment: Fragment, gameManager: ControllerFindPairGame?) =
+    fun startDialogPauseGameFindPair(
+        fragment: Fragment,
+        gameManager: ControllerFindPairGame?
+    ) =
         showDialog(
+            fragment.requireActivity(),
             fragment, R.layout.dialog_game_find_pair_pause
         ) { gameManager?.stopGame() }
 
-    fun startDialogVictoryGameFindPair(fragment: Fragment, gameManager: ControllerFindPairGame?) =
+    fun startDialogVictoryGameFindPair(
+        fragment: Fragment,
+        gameManager: ControllerFindPairGame?
+    ) =
         showDialog(
+            fragment.requireActivity(),
             fragment, R.layout.dialog_game_find_pair_victory
         ) { gameManager?.stopGame() }
 
     fun startDialogLoseGamePuzzle(fragment: Fragment) = showDialog(
+        fragment.requireActivity(),
         fragment, R.layout.dialog_game_lose
     ) { restartRound() }
 
     fun startDialogPauseGamePuzzle(fragment: Fragment) = showDialog(
+        fragment.requireActivity(),
         fragment, R.layout.dialog_game_pause
     ) { restartRound() }
 
     fun startDialogVictoryGamePuzzle(fragment: Fragment) = showDialog(
+        fragment.requireActivity(),
         fragment, R.layout.dialog_game_victory
     ) { restartRound() }
 
     fun startDialogLoseGameQuiz(fragment: Fragment, onDismiss: () -> Unit) =
-        showDialog(fragment, R.layout.dialog_game_find_pair_lose, onDismiss)
+        showDialog(fragment.requireActivity(), fragment, R.layout.dialog_game_find_pair_lose, onDismiss)
 
     fun startDialogVictoryGameQuiz(fragment: Fragment, onDismiss: () -> Unit) =
-        showDialog(fragment, R.layout.dialog_game_find_pair_victory, onDismiss)
+        showDialog(fragment.requireActivity(), fragment, R.layout.dialog_game_find_pair_victory, onDismiss)
 }
